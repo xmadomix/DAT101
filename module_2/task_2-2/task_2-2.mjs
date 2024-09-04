fc82a2e8869b2de3aebb9729a23bff03b9816b91
xmadomix <monipett@online.no>
2024-09-04 16:02:19 +0200

    test

--- module_2/task_2-2/task_2-2.mjs
+++ module_2/task_2-2/task_2-2.mjs
@@ -4,6 +4,7 @@ initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+//testetetdgtg
 printOut("Replace this with you answer!");
 printOut(newLine);
 
