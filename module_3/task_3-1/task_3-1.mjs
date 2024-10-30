1f542f566b74a0e7d621d82f9823c6137d63ab31
xmadomix <monipett@online.no>
2024-10-30 21:44:01 +0100

    task 3.1 :)

--- module_3/task_3-1/task_3-1.mjs
+++ module_3/task_3-1/task_3-1.mjs
@@ -4,40 +4,90 @@ initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Task 1, 2 and 3");
-let wakeUpTime = 2;
-if ("Replace this") {
-  printOut("I can take the bus to school.");
+
+let wakeUpTime = 7 ;
+
+if (wakeUpTime === 7) {
+  printOut ("If I wake up at exactly 7 o'clock then I can catch the bus to school.") ; 
+}
+else if (wakeUpTime === 8) { 
+    printOut ("If I wake up at exactly 7 o'clock, I can take the bus to school, otherwise if I wake up exactly at 8 o'clock, I can take the train to school, otherwise I have to take the car to school") ;
+}  
+else {
+  printOut ("I wont be able to catch the bus to school.") ;
 }
 printOut(newLine);
 
+
+
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let number = 0;
+number = parseInt(number) ; 
+
+if (number > 0) {
+  printOut ("positive") ;
+} 
+else if (number < 0) {
+  printOut ("negative") ;
+}
+else {
+  printOut ("zero") ; 
+}
 printOut(newLine);
 
-printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
+
+
+printOut("--- Part 6, 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let imageSize = Math.floor (Math.random() * 8) + 1 ;
+printOut ("Uploaded image size:", imageSize, "MP");
+
+let minSize = 4;
+let maxSize = 6;
+
+if ( imageSize  >= maxSize ) {
+  printOut ("Image is too large") ;
+} else if ( imageSize <= minSize ) {
+  printOut ("Thank you") ;
+} else {
+  printOut ("The Image is too small") ;
+}
 printOut(newLine);
 
-printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
+
+
+printOut("--- Part 8, 9, 10 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const monthList = ["January", "February", "Mars", "April", "Mai",
+  "June", "July", "August", "September", "October", "November", "December"] ;
+const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] ;
+const noOfMonth = monthList.length;
+const monthName = monthList[Math.floor(Math.random() * noOfMonth)];
+
+
+printOut("Selected month:" + monthName ) ;
+
+if (monthName === "Mars" || monthName === "Mai" ) {
+  printOut ("The gallery is closed.") ;
+} else if (monthName === "April") {
+  printOut("The gallery is open in temporary premises.") ;
+} else {
+  printOut("The gallery is open.") ;
+}
+
+  if ( monthName.includes("r") ) {
+    printOut ("You must take vitamin D") ;
+  } else {
+    printOut ("You do not need to take vitamin D") ;
+  }
+
+const monthIndex = monthList.indexOf(monthName) ;
+const days = daysInMonth[monthIndex] ;
+printOut ("Number of days in: " + monthName + " is " + days) ;
+
 printOut(newLine);
 
-printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
-
-printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
-
-/* Task 10*/
-printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
