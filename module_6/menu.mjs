c51bc1ac4598da50e2799a4c57d72148fae50cfe
xmadomix <monipett@online.no>
2025-05-12 20:53:19 +0200

    snakeParty

---
+++ module_6/menu.mjs
@@ -0,0 +1,12 @@
+"use strict";
+
+/* Use this file to create the menu for the snake game. */
+
+import libSprite from "../../common/libs/libSprite_v2.mjs";
+import lib2D from "../../common/libs/lib2d_v2.mjs";
+import libSprite from "../../common/libs/libSprite.mjs";
+
+
+export class TMenu {
+    
+}
